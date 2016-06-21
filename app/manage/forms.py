a0c8eb3f22ba312a3af93af64c0c5f4bc7db2a68
mapan1984 <mapan1984@outlook.com>
2016-06-21 17:50:24 +0800

    完成添加/删除电脑表格界面

---
+++ app/manage/forms.py
@@ -0,0 +1,13 @@
+from flask.ext.wtf import Form
+from wtforms import StringField, SubmitField
+from wtforms.validators import Required, Length
+from ..models import Computer 
+
+
+class AddForm(Form):
+    name = StringField('电脑编号', validators=[Required(), Length(1, 64)])
+    submit = SubmitField('添加')
+
+class DelForm(Form):
+    name = StringField('电脑编号', validators=[Required(), Length(1, 64)])
+    submit = SubmitField('删除')
--- app/manage/views.py
+++ app/manage/views.py
@@ -2,23 +2,29 @@ from flask import render_template, redirect, request, url_for, flash
 from flask.ext.login import login_user, logout_user, login_required
 from . import manage
 from ..models import User, Computer
-
+from .forms import AddForm, DelForm
 
 # prefix 为注册路由自动加前缀/manage
 @manage.route('/busy_computers')
 @login_required  # flask-login提供的修饰器，保护路由只能由登陆用户访问
 def busy_computers():
-    return render_template('manage/busy_computers.html', computer_list=Computer.query.all())
+    return render_template('manage/busy_computers.html', 
+                           computer_list=Computer.query.all())
 
 @manage.route('/free_computers')
 @login_required  # flask-login提供的修饰器，保护路由只能由登陆用户访问
 def free_computers():
-    return render_template('manage/free_computers.html', computer_list=Computer.query.all())
+    return render_template('manage/free_computers.html', 
+                           computer_list=Computer.query.all())
 
-@manage.route('/add_del_computers')
+@manage.route('/add_del_computers', methods=['GET', 'POST'])
 @login_required  # flask-login提供的修饰器，保护路由只能由登陆用户访问
 def add_del_computers():
-    return render_template('manage/add_del_computers.html', computer_list=Computer.query.all())
+    add_form = AddForm()
+    del_form = DelForm()
+    return render_template('manage/add_del_computers.html', 
+                           computer_list=Computer.query.all(),
+                           add_form=add_form, del_form=del_form)
 
 @manage.route('/logout')
 @login_required  # flask-login提供的修饰器，保护路由只能由登陆用户访问

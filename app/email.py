4075ee1c4db976d1e601fee6f44b2ca735709ff9
mapan1984 <mapan1984@outlook.com>
2016-06-21 08:52:56 +0800

    完成管理页面的html布局，为填充内容做准备

--- app/email.py
+++ app/email.py
@@ -9,7 +9,7 @@ def send_async_email(app, msg):
         mail.send(msg)
 
 def send_email(to, subject, template, **kwargs):
-    app = current_app._get_current_object()
+    app = current_app._get_current_object()  # 获取current_app
     msg = Message(app.config['MAIL_SUBJECT_PREFIX'] + subject,
                   sender=app.config['MAIL_SENDER'], recipients=[to])
     msg.body = render_template(template + '.txt', **kwargs)

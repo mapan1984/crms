ecb2d9fd429b32e00ecafbb7e0687e642ef6c56c
mapan1984 <mapan1984@outlook.com>
2016-06-30 10:32:18 +0800

    增加修改价格

--- init_db.py
+++ init_db.py
@@ -9,10 +9,13 @@ from app.models import User, Computer
 db.drop_all()
 db.create_all()
 
-admin_mapan = User(email='mapansky1984@163.com', username='mapan', is_admin=True)
-admin_mapan.password = 'mapan'
-user_other = User(email='2642896890@qq.com', username='other', is_admin=False)
-user_other.password = 'other'
+admin_mapan = User(email='mapansky1984@163.com', password='mapan', username='马攀', is_admin=True)
+user_ben = User(email='1984@sian.com', password='ben', username='王伟', is_admin=False)
+user_1ther = User(email='lisi@qq.com', password='lisi', username='李四', is_admin=False)
+user_2ther = User(email='zhangwei@qq.com', password='zhangwei', username='张伟', is_admin=False)
+user_3ther = User(email='liming@qq.com', password='liming', username='李明', is_admin=False)
+user_4ther = User(email='wangyong@qq.com', password='wangyong', username='王勇', is_admin=False)
+user_5ther = User(email='zhangjun@qq.com', password='zhangjun', username='张军', is_admin=False)
 
 c1 = Computer(name='c1')
 c2 = Computer(name='c2')
@@ -28,11 +31,11 @@ c5.memo = "序列号：PF035E9C 处理器：AMD A8-6410 APU with AMD Radeon R5 G
 c6.memo = "序列号：PF035E9C 处理器：AMD A8-6410 APU with AMD Radeon R5 Graphics 2.00GHZ 产品编号：80E1 BIOS: A2CN24WWV1.06 Lenovo G4045 Windows 10 Home China (x64)"
 
 # 通过db.session管理数据库的改动
-db.session.add_all([c1, c2, c3, c4, c5, c6, admin_mapan, user_other])
+db.session.add_all([c1, c2, c3, c4, c5, c6, admin_mapan, user_ben,\
+                    user_1ther,user_2ther,user_3ther,user_4ther,user_5ther])
 db.session.commit()
 
 print(c1.id)
-
 
 f=True
 
